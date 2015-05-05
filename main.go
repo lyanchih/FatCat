5d1cb82257520caf0a2d08a6deadd8736a795e51
Chung Chih, Hung <lyanchih@gmail.com>
2015-05-05 21:44:39 +0800

    Simple mega downloader first.

--- main.go
+++ main.go
@@ -2,15 +2,62 @@ package main
 
 import (
   "./mega"
+  "./task"
+  "os"
   "fmt"
+  "log"
+  "io/ioutil"
 )
 
-func main() {
-  url := "https://mega.co.nz/#!sQ4HBA6a!md9EZHtkl_-A9hSJIy2KR4_4tFAZ_1p5dxhE2_KBcjE"
-  m := mega.New(url, "tmp")
-  m.Download()
+const MaxWorker = 10
 
-  if m.Error() != nil {
-    fmt.Println(m)
+// var sample []mega.MegaInfo = []mega.MegaInfo{
+//   {"https://mega.co.nz/#!sQ4HBA6a!md9EZHtkl_-A9hSJIy2KR4_4tFAZ_1p5dxhE2_KBcjE", "", 1},
+//   {"https://mega.co.nz/#!sQ4HBA6a!md9EZHtkl_-A9hSJIy2KR4_4tFAZ_1p5dxhE2_KBcjE", "", 2},
+// }
+
+func main() {
+  if len(os.Args) == 0 {
+    log.Fatal("Please offer json file.")
   }
+  pool, err := task.CreatePool()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  pool.Registry(task.MegaType, mega.MegaRoot{})
+
+  file, err := os.Open(os.Args[1])
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer file.Close()
+  
+  data, err := ioutil.ReadAll(file)
+  if err != nil {
+    log.Fatal(err)
+  }
+  
+  _, err = pool.Add(task.MegaType, file.Name(), data)
+  if err != nil {
+    log.Fatal(err)
+  }
+  
+  go pool.Start()
+
+  for i := 0; i < MaxWorker; i++{
+    go func(i int) {
+      fmt.Println("Process", i, "asking...")
+      t, ok := pool.Ask()
+      if ok {
+        fmt.Println("Process", i, "downloading...")
+        t.Download()
+        fmt.Println("Process", i, "reporting...")
+        pool.Report(t)
+        fmt.Println("Process", i, "done")
+      }
+    }(i)
+  }
+
+  select{}
 }
